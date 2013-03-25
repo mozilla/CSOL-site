5ef836c7c0ed838acf75565b9d13d67c0513e2bd
Brian J Brennan <brianloveswords@gmail.com>
2013-03-25 10:56:30 -0400

    Add nunjucks, package.json

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const app = express();
+const nunjucks = require('nunjucks');
+
+const env = new nunjucks.Environment(new nunjucks.FileSystemLoader(path.join(__dirname, 'views')));
+env.express(app);
 
 app.use(express.logger());
 app.use(express.compress());
@@ -11,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'static')));
 require('./controllers/auth')(app);
 require('./controllers/info')(app);
 require('./controllers/backpack')(app);
-require('./controllers/make')(app);
+require('./controllers/program')(app);
 
 if (!module.parent)
   app.listen(3000);
