5080673f6613072df2f0d6834e74aebe4e20d828
Andrew Hayward <github@andrewhayward.net>
2013-04-18 15:33:41 +0100

    Sign-up process

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,6 +6,36 @@ var bcrypt = require('bcrypt'),
 
 module.exports = function (app) {
 
+  var validateEmail = function (email) {
+    // TODO - make sure email is valid
+    return true;
+  }
+
+  var normalizeUsername = function (username) {
+    // For now, just remove white space and lower case it
+    return (''+username).replace(/\s/g, '').toLowerCase();
+  }
+
+  var validateUsername = function (username) {
+    // TODO - make sure username is valid
+    return true;
+  }
+
+  var generatePassword = function () {
+    return 'GeneratedPassword';
+  }
+
+  var validatePassword = function (password) {
+    // TODO - make sure password is valid
+    return true;
+  }
+
+  var generateToken = function () {
+    // There must be better ways of doing it than this!
+    var now = Date.now();
+    return ((Math.random() * now).toString(36) + '-' + (Math.random() * now).toString(36)).replace(/[^a-z0-9-]/ig, '-');
+  }
+
   var extractUserData = function (user) {
     var userType = user.daoFactoryName.toLowerCase(),
         userHome;
@@ -41,6 +71,7 @@ module.exports = function (app) {
   });
 
   app.get('/login', function (req, res, next) {
+    console.log(generateToken());
     if (req.session.user) {
       return res.redirect('/backpack', 303);
     } else {
@@ -67,18 +98,18 @@ module.exports = function (app) {
       if (user) {
         bcrypt.compare(password, user.password, function(err, match) {
           if (err || !match) {
-            finalize(new Error(err || 'Username or password incorrect'));
+            finalize(new Error(err || 'Nickname or password incorrect'));
           } else {
             finalize(null, user);
           }
         });
       } else {
-        finalize(new Error('Username or password incorrect'));
+        finalize(new Error('Nickname or password incorrect'));
       }
     }
 
     if (!username || !password) {
-      finalize(new Error('Missing username or password'));
+      finalize(new Error('Missing nickname or password'));
     } else {
       // Annoying redundancy here, but no other obvious way to generate OR queries
       learners.find({where: ["`email`=? OR `username`=?", username, username]}).success(function(user) {
@@ -104,56 +135,148 @@ module.exports = function (app) {
   });
 
   app.get('/signup/learners', function (req, res, next) {
-    var user = req.session.user || {};
+    var signup = req.session.signup || {};
+    console.log(signup);
 
-    if (user.needs_guardian) {
-      req.session.signup_step = 'child';
-      res.render('auth/signup-child.html', {
-        password: '{Generated Password}'
-      });
-    } else if (user.valid !== undefined) {
-      req.session.signup_step = 'more';
-      res.render('auth/signup-learner-more.html', {
-        password: '{Generated Password}'
-      });
+    if (signup.state == 'child') {
+      res.render('auth/signup-child.html', signup);
+    } else if (signup.state == 'more') {
+      res.render('auth/signup-learner-more.html', signup);
     } else {
-      delete req.session.signup_step;
+      delete req.session.signup;
       res.render('auth/signup-learner.html');
     }
   });
 
   app.post('/signup/learners', function (req, res, next) {
-    var step = req.session.signup_step;
+    var signup = req.session.signup || {};
+    delete signup.errors;
 
-    if (!step) {
-      var year = req.body['birthday-year'],
-          month = req.body['birthday-month'],
-          day = req.body['birthday-day'],
-          birthday = new Date(year, month, day),
-          today = new Date(),
-          cutoff = new Date(today.getFullYear() - 13, today.getMonth(), today.getDate());
+    if (signup.state == 'child') {
+      var normalizedUsername = normalizeUsername(signup.username);
 
-      req.session.user = {
-        username: req.body['username'],
-        is_underage: birthday > cutoff,
-        type: 'learner',
-        favorites: []
+      signup.parent_email = req.body['parent_email'];
+
+      var fail = function(err) {
+        signup.errors = [err];
+        res.session.signup = signup;
+        res.render('auth/signup-child.html', signup);
       }
 
-      req.session.signup_step = 1;
-      req.session.user.valid = false;
-      if (req.session.user.is_underage) {
-        req.session.user.needs_guardian = true;
-      }
-      return res.redirect('/signup/learners', 303);
-    } else if (step == 'child') {
-      
-    } else if (step == 'more') {
+      if (!signup.parent_email) {
+        fail(new Error('Missing email address'));
+      } else if (!validateEmail(signup.parent_email)) {
+        fail(new Error('Invalid email address'));
+      } else {
+        learners.find({where: {username: normalizedUsername}}).success(function(user) {
+          signupTokens.create({
+            email: signup.parent_email,
+            token: generateToken(),
+          }).success(function(token) {
+            // TODO - send an email
 
+            console.log(token.token);
+            token.setLearner(user); // Assuming this worked
+
+            bcrypt.hash(signup.password, 10, function(err, hash) {
+              user.updateAttributes({
+                complete: true,
+                password: hash
+              }).success(function() {
+                delete req.session.signup;
+                redirectUser(req, res, user);
+              });
+            });
+          });
+        });
+      }
+    } else if (signup.state == 'more') {
+      var normalizedUsername = normalizeUsername(signup.username);
+
+      signup.email = req.body['email'];
+      if ('password' in req.body) signup.password = req.body['password'];
+
+      var fail = function(err) {
+        signup.errors = [err];
+        res.session.signup = signup;
+        res.render('auth/signup-learner-more.html', signup);
+      }
+
+      if (!signup.email || !signup.password) {
+        fail(new Error('Missing email address or password'));
+      } else if (!validateEmail(signup.email)) {
+        fail(new Error('Invalid email address'));
+      } else if (!validatePassword(signup.password)) {
+        fail(new Error('Invalid password'));
+      } else {
+        learners.find({where: {username: normalizedUsername}}).success(function(user) {
+          bcrypt.hash(signup.password, 10, function(err, hash) {
+            user.updateAttributes({
+              complete: true,
+              email: signup.email,
+              password: hash
+            }).success(function() {
+              delete res.session.signup;
+              redirectUser(req, res, user);
+            });
+          });
+        });
+      }
+    } else {
+      signup.birthday_year = parseInt(req.body['birthday_year'], 10);
+      signup.birthday_month = parseInt(req.body['birthday_month'], 10);
+      signup.birthday_day = parseInt(req.body['birthday_day'], 10);
+      signup.username = req.body['username'];
+
+      var normalizedUsername = normalizeUsername(signup.username);
+
+      var birthday = new Date(
+        signup.birthday_year,
+        signup.birthday_month - 1, // JS dates have 0-indexed months
+        signup.birthday_day
+      );
+
+      var today = new Date();
+      var cutoff = new Date(
+        today.getFullYear() - 13,
+        today.getMonth(),
+        today.getDate()
+      );
+
+      var fail = function(err) {
+        signup.errors = [err];
+        req.session.signup = signup;
+        res.render('auth/signup-learner.html', signup);
+      }
+
+      // Check for accidental February 30ths, etc
+      if (birthday.getFullYear() !== signup.birthday_year
+            || birthday.getMonth() !== signup.birthday_month - 1
+            || birthday.getDate() !== signup.birthday_day) {
+        fail(new Error('This is not a valid date.'));
+      } else {
+        learners.find({where: {username: normalizedUsername}}).success(function(user) {
+          if (user) {
+            fail(new Error('This nickname is already in use'));
+          } else {
+            // Create the user now, to prevent race conditions on usernames
+            var underage = (birthday > cutoff);
+
+            learners.create({
+              username: normalizedUsername,
+              password: '',
+              underage: underage
+            }).success(function(user) {
+              console.log(user);
+              signup.state = underage ? 'child' : 'more';
+              signup.password = generatePassword();
+              req.session.signup = signup;
+              res.redirect('/signup/learners', 303);
+            });
+          }
+        });
+      }
     }
-
-    req.session.user.is_valid = true;
-    return res.redirect('/backpack', 303);
   });
 
   app.get('/signup/parents', function (req, res, next) {
@@ -177,6 +300,10 @@ module.exports = function (app) {
 
     if (!email || !password) {
       finalize(new Error('Missing email or password'));
+    } else if (!validateEmail(email)) {
+      finalize(new Error('Invalid email address'));
+    } else if (!validatePassword(password)) {
+      finalize(new Error('Invalid password'));
     } else {
       guardians.find({where: {email: email}}).success(function(user) {
         if (user) {
