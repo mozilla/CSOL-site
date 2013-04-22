cbda6e6c1d745964a57ef845e7bc5b93422f7f4b
Andrew Hayward <github@andrewhayward.net>
2013-04-22 12:07:08 +0100

    Simplifying `learn` controller
    Removing all references to models, etc.

--- controllers/learn.js
+++ controllers/learn.js
@@ -1,84 +1,135 @@
+var querystring = require('querystring');
+
 module.exports = function(app) {
 
-    app.param('section', function(req, res, next, section) {
-        if (section == 'programs') {
-            req.params.section = {
-                title: 'Programs',
-                model: 'program'
-            };
-        } else if (section == 'orgs') {
-            req.params.section = {
-                title: 'Organizations',
-                model: 'organization'
-            };
-        } else if (section == 'activities') {
-            req.params.section = {
-                title: 'Activities',
-                model: 'activity'
-            };
-        } else if (section == 'games') {
-            req.params.section = {
-                title: 'Games',
-                model: 'game'
-            };
-        } else {
-            return next(new Error('Learn section "' + section + '" not found'));
-        }
+  app.param('section', function(req, res, next, section) {
+    if (section == 'programs') {
+      req.params.section = {
+        title: 'Programs',
+        set: 'program'
+      };
+    } else if (section == 'orgs') {
+      req.params.section = {
+        title: 'Organizations',
+        set: 'organization'
+      };
+    } else if (section == 'activities') {
+      req.params.section = {
+        title: 'Activities',
+        set: 'activity'
+      };
+    } else if (section == 'games') {
+      req.params.section = {
+        title: 'Games',
+        set: 'game'
+      };
+    } else {
+      return next(new Error('Learn section "' + section + '" not found'));
+    }
 
-        next();
+    req.params.section.path = section;
+
+    next();
+  });
+
+  app.param('item_id', function(req, res, next, id) {
+    var section = req.params.section;
+
+    // TODO - hook this into a model somewhere
+    req.params.item = {};
+
+    next();
+  });
+
+  app.get('/learn/:section?/:item_id?', function(req, res) {
+    var section = req.params.section;
+    var item = req.params.item;
+
+    var items = [];
+
+    var itemOptions = [{
+      thumbnail: '/media/images/org.png',
+      description: 'Organisation blah irure...',
+      url: '/orgs/some-organisation'
+    },{
+      thumbnail: '/media/images/program.png',
+      description: 'Program blah sed eiusmod...',
+      url: '/programs/ae784f'
+    },{
+      thumbnail: '/media/images/challenge.png',
+      description: 'Challenge id est laborum...',
+      url: '/challenges/some-challenge'
+    }];
+
+    if (item)
+      return res.render('learn/' + section.path + '/single.html', {
+        item: item
+      });
+
+    console.log(req.query);
+    if ('category' in req.query) {
+      var category = req.query['category'];
+      delete req.query['category'];
+      var query = querystring.stringify(req.query);
+      return res.redirect('/learn/' + category + '?' + query);
+    }
+
+    for (var i = 0; i < 12; ++i) {
+      items.push(itemOptions[Math.floor(Math.random()*itemOptions.length)]);
+    }
+
+    var filters = [{
+      name: 'category',
+      label: 'Category',
+      selected: (section || {}).path,
+      options: {
+        activities: 'Activities',
+        orgs: 'Organizations',
+        programs: 'Programs'
+      }
+    },{
+      name: 'age',
+      label: 'Age',
+      options: {}
+    },{
+      name: 'topic',
+      label: 'Topic',
+      options: {}
+    },{
+      name: 'date',
+      label: 'Date',
+      options: {}
+    },{
+      name: 'search',
+      label: 'Search',
+      value: '',
+      type: 'search'
+    }];
+
+    if (section)
+      return res.render('learn/' + section.set + '/list.html', {
+        filters: filters,
+        pageTitle: section.title,
+        items: items
+      });
+
+    res.render('learn/list.html', {
+      filters: filters,
+      items: items
     });
+  });
 
-    app.param('item_id', function(req, res, next, id) {
-        var section = req.params.section,
-            model = require('../models/' + section.model);
+  app.get('/learn/:section/:item_id/:action', function(req, res, next) {
+    var item = req.params.item;
+    var action = req.params.action;
 
-        model.find(id).success(function(item) {
-            if (item) {
-                req.params.item = item;
-                next();
-            } else {
-                next(new Error('<' + section.model + ': ' + id + '> not found'));
-            }
-        });
-    });
+    if (action === 'favorite')
+      return res.send('Favoriting ' + item);
 
-    app.get('/learn/:section?/:item_id?', function(req, res) {
-        var section = req.params.section,
-            item = req.params.item;
+    if (action === 'unfavorite')
+      return res.send('Unfavoriting ' + item);
 
-        if (item) {
-            res.render('learn/' + section.model + '/single.html', {
-                item: item
-            })
-        } else if (section) {
-            var model = require('../models/' + section.model);
-
-            model.all().success(function(items) {
-                res.render('learn/' + section.model + '/list.html', {
-                    items: items || []
-                });
-            });
-        } else {
-            res.render('learn/list.html', {
-                items: []
-            });
-        }
-    });
-
-    app.get('/learn/:section/:item_id/:action', function(req, res, next) {
-        var item = req.params.item,
-            action = req.params.action;
-
-        switch (action) {
-            case 'favorite':
-                res.send('Favoriting ' + item);
-                break;
-            case 'unfavorite':
-                res.send('Unfavoriting ' + item);
-                break;
-            default:
-                next();
-        }
-    });
+    next();
+  });
 
 }
