870a2300816f7385720e244f913f9f107a70f039
Mike Larsson <mikelarssonftw@gmail.com>
2013-05-23 15:44:57 -0400

    Fixes various bugs in mandrill.js Adds more detailed logging to mandrill.js Send confirmation emails on learner account signup.

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,8 @@ var bcrypt = require('bcrypt');
 var passwords = require('../lib/passwords');
 var usernames = require('../lib/usernames');
 var db = require('../db');
+var email = require('../mandrill');
+var logger = require('../logger');
 var learners = db.model('Learner');
 var guardians = db.model('Guardian');
 var signupTokens = db.model('SignupToken');
@@ -178,6 +180,7 @@ function processChildLearnerSignup (req, res, next) {
           }).complete(function(err) {
             if (err) return fail(err);
 
+            email.send('<13 learner signup', {}, signup.parent_email);
             delete req.session.signup;
             req.flash('modal', {
               title: 'Welcome to the Chicago Summer of Learning',
@@ -239,6 +242,7 @@ function processStandardLearnerSignup (req, res, next) {
             return fail(err);
           }
 
+          email.send('learner signup', {}, signup.email);
           delete req.session.signup;
           redirectUser(req, res, user);
         });
