a154ee2a786860331907d0e4aa9e0d8247b27b31
Cory Shaw <coryshaw@gmail.com>
2013-05-23 10:28:52 -0600

    added pinterest style behaivor to modal windows

--- static/media/js/custom_logic.js
+++ static/media/js/custom_logic.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
 	$('.show-tooltip').tooltip();
+
+
+	$('.modal').on('show', function () {
+		$('body').addClass('noscroll');
+		$('.modal-container').show();
+	});
+
+	$('.modal').on('hide', function () {
+		$('body').removeClass('noscroll');
+		$('.modal-container').hide();
+	});
+
 });
