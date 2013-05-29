ff889ee14f915f58a7de874b0ad746ebe545f31a
Michael Ballard <michael@ballard.is>
2013-05-29 11:16:25 -0400

    rahm's head

--- static/media/js/custom_logic.js
+++ static/media/js/custom_logic.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
 	$('.show-tooltip').tooltip();
+	if($('body.home').length != 0) {
+		$('<img style="position:absolute; top:0; left:0;" src="/media/img/rahm-head.png">').prependTo('.footer .upper');
+	}
 });
