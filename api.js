594c9f178a532b61009c398c3c0c68b69d35ed81
Paul Smith <paulsmith@pobox.com>
2013-06-04 21:46:32 -0400

    Middleware helper to support multiple API calls
    Currently the 'middleware()' helper in the api module sets the result of an
    API call to the 'req.remote' property. Effectively this allows a view to only
    make one API call using the helper, because subsequent ones will clobber
    whatever's in 'req.remote'.
    
    This change extends the object, so long as the data objects returned from API
    calls use different keys.

--- api.js
+++ api.js
@@ -44,7 +44,9 @@ function middleware (method, default_query) {
         return res.json(data);
       }
 
-      req.remote = data;
+			// Mutiple API calls are supported: to prevent clobbering existing data,
+			// use different keys for each call in the 'data' object.
+      req.remote = _.extend(req.remote || {}, data);
 
       if (err)
         return next(err);
