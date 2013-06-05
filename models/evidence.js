cbcb98313ac00593caad046e4fe67731b9ca2e97
Andrew Hayward <github@andrewhayward.net>
2013-06-05 04:37:56 +0100

    Moving file processing to evidence model

--- models/evidence.js
+++ models/evidence.js
@@ -1,53 +1,73 @@
 var db = require('../db');
+var errors = require('../lib/errors');
+var mime = require('mime');
+var path = require('path');
 var s3 = require('../s3');
+var thumbs = require('../lib/thumbs');
 var _ = require('underscore');
 
 var MIME_MAP = {
   'image/*':
-    thumbs.image,
-  'audio/*':
-    staticThumb('audio'),
+    'image',
   'video/*':
-    thumbs.video,
-  'text/*':
-    staticThumb('text'),
+    'video',
+  'audio/*':
+    'audio',
+  'text/plain':
+    'text',
   'application/pdf':
-    staticThumb('pdf'),
-  'application/postscript':
-    staticThumb('postscript'),
+    'pdf',
   'application/msword':
-    staticThumb('ms-text'),
+    'ms-document',
   'application/mspowerpoint':
-    staticThumb('ms-presentation'),
+    'ms-presentation',
   'application/vnd.ms-word':
-    staticThumb('ms-text'),
+    'ms-document',
   'application/vnd.ms-powerpoint':
-    staticThumb('ms-presentation'),
+    'ms-presentation',
   'application/vnd.ms-excel':
-    staticThumb('ms-spreadsheet'),
+    'ms-spreadsheet',
   'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
-    staticThumb('ms-text'),
+    'ms-document',
   'application/vnd.openxmlformats-officedocument.presentationml.presentation':
-    staticThumb('ms-presentation'),
+    'ms-presentation',
   'application/vnd.oasis.opendocument.text':
-    staticThumb('od-text'),
+    'document',
   'application/vnd.oasis.opendocument.spreadsheet':
-    staticThumb('od-spreadsheet'),
+    'spreadsheet',
   'application/vnd.oasis.opendocument.presentation':
-    staticThumb('od-presentation'),
+    'presentation',
   'application/vnd.oasis.opendocument.graphics':
-    staticThumb('od-drawing')
+    'drawing'
 };
 
 var ALLOWED_TYPES = _.keys(MIME_MAP);
 
 var MEDIA_TYPE_TEST = new RegExp(
-  '^(' + ALLOWED_TYPES.join('|').replace(/\//g,'\\/').replace(/\*/g,'[^/]+') + ')$',
+  '^\s*(' + reFormat(ALLOWED_TYPES.join('|')) + ')\s*$',
   'gi'
 );
 
-function staticThumb (name) {
-  return '/media/thumbs/' + name + '.png';
+function reFormat (type) {
+  return type
+    .replace(/\//g, '\\/')
+    .replace(/\*/g, '[^\\/]+')
+    .replace(/\./g, '\\.');
+}
+
+function getStaticThumbPath (type) {
+  return path.join(__dirname, '../thumbs/' + type + '.png');
+}
+
+function getSimpleType (mimeType) {
+  return _.find(MIME_MAP, function (type, mime) {
+    return (new RegExp('^' + reFormat(mime) + '$', 'ig'))
+      .test(mimeType);
+  })
+}
+
+function isValidType (type) {
+  return MEDIA_TYPE_TEST.test(type);
 }
 
 module.exports = {
@@ -67,8 +87,8 @@ module.exports = {
       allowNull: false,
       validate: {
         isAllowedType: function (value) {
-          if (!MEDIA_TYPE_TEST.test(value))
-            throw new Error('Not a supported file type'))
+          if (!isValidType(value))
+            throw new Error('"' + value + '" is not a supported file type');
         }
       }
     },
@@ -104,6 +124,9 @@ module.exports = {
       // Returns an HTTP(S) request
       callback(s3.get(this.thumbnail));
     },
+    getSimpleType: function () {
+      return getSimpleType(this.mediaType);
+    },
     getLocationUrl: function () {
       return '/evidence/' + this.key;
     },
@@ -114,6 +137,87 @@ module.exports = {
   classMethods: {
     getMimeTypes: function () {
       return ALLOWED_TYPES;
+    },
+    isValidType: isValidType,
+    process: function (file, remotePrefix, callback) {
+      console.log(file);
+
+      if (!file || !file.size)
+        return callback();
+
+      if (!isValidType(file.type)) {
+        return callback(new errors.Unsupported(file.name + ' is not of a supported file type'));
+      }
+
+      if (_.isFunction(remotePrefix)) {
+        callback = remotePrefix;
+        remotePrefix = null;
+      }
+
+      remotePrefix = remotePrefix || 'uploads';
+
+      function put (local, remote, type, callback) {
+        var method = _.isString(local) ? 'putFile' : 'putBuffer';
+
+        s3[method](local, remote, {
+          'Content-Type': type
+        }, callback);
+      }
+
+      function saveFile (path, callback) {
+        put(file.path, path, file.type, callback);
+      }
+
+      function saveThumbnail (path, callback) {
+        var type = getSimpleType(file.type);
+
+        if (!thumbs[type]) {
+          var thumbPath = getStaticThumbPath(type);
+          var thumbType = mime.lookup(thumbPath);
+
+          return put(thumbPath, path, thumbType, callback);
+        }
+
+        thumbs[type](file.path, null, 'png', 150, 150, function (err, data) {
+          if (err)
+            return callback(err);
+
+          put(new Buffer(data, 'binary'), path, 'image/png', callback);
+        });
+      }
+
+      var Model = this;
+      var key = file.path.split('/').pop();
+      var format = mime.extension(file.type) || path.extname(file.name).substr(1);
+      var remoteFilePath = '/' + remotePrefix + '/' + key + '.' + format;
+      var remoteThumbPath = '/' + remotePrefix + '/' + key + '_thumb.' + format;
+
+      saveThumbnail(remoteThumbPath, function (err) {
+        if (err)
+          return callback(err);
+
+        Model.create({
+          key: key,
+          mediaType: file.type,
+          location: remoteFilePath,
+          thumbnail: remoteThumbPath,
+          original: file.name
+        }).complete(function (err, instance) {
+          if (err || !instance)
+            return callback(err);
+
+          saveFile(remoteFilePath, function (err) {
+            if (err)
+              return;
+
+            instance.updateAttributes({
+              saved: true
+            });
+          });
+
+          callback(null, instance);
+        });
+      });
     }
   }
 };
