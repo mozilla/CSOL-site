1668cb681b1cfe51390668129bc0c3b0805c108f
Andrew Hayward <github@andrewhayward.net>
2013-06-18 21:48:49 +0100

    Adding email validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,7 +44,9 @@ function getFullUrl(origin, path) {
 }
 
 function validateEmail (email) {
-  // TODO - make sure email is valid
+  if (!email.match(/^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/)) {
+      return 'Invalid email address';
+  }
   return true;
 }
 
